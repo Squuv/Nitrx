da2183c65954dfbe72362ae375b466e7c01a9060
JBrahim <53058224+BrahimJarrar@users.noreply.github.com>
2019-11-09 16:33:49 +0100

    💧 Bash Install.sh

--- install.sh
+++ install.sh
@@ -9,20 +9,17 @@ off="\e[0m"
 function banner(){
     echo -e "======= NITRX INSTALL ======="
 }
-function app() {
-    echo -e "$red [$green+$red]$off Installing Packages ...";
-    pip install -r ./requirements.txt
-    echo -e "$red [$green+$red]$off Checking directories ..."
-    git pull origin master
-    echo -e "$red [$green+$red]$off Installing ...";
-    chmod +x Nitrx.py
-    echo -e "$red [$green+$red]$off Tool successfully installed and will start in 5s!";
-    echo -e "$red [$green+$red]$off You can execute tool by typing python Nitrx.py";
-    sleep 5;
-    exit
-}
 
 #main
 banner
 echo -e "$red [$green+$red]$off Nitrx Will Be Installed In Your System";
-app
+echo -e "$red [$green+$red]$off Installing Packages ...";
+pip install -r ./requirements.txt
+echo -e "$red [$green+$red]$off Checking directories ..."
+git pull origin master
+echo -e "$red [$green+$red]$off Installing ...";
+chmod +x Nitrx.py
+echo -e "$red [$green+$red]$off Tool successfully installed and will start in 5s!";
+echo -e "$red [$green+$red]$off You can execute tool by typing python Nitrx.py";
+sleep 5;
+python Nitrx.py
