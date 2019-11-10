c4cc61dba3723f2e7789ee8952e758daff7b5eb4
JBrahim <53058224+BrahimJarrar@users.noreply.github.com>
2019-11-10 14:35:15 +0100

    💧 Bash Install

--- install.sh
+++ install.sh
@@ -4,19 +4,13 @@ red="\e[0;31m"
 blue="\e[0;94m"
 green="\e[0;32m"
 off="\e[0m"
-# Android.Termux.Orusula
-
-function banner(){
-    echo -e "======= NITRX INSTALL ======="
-}
 
 #main
-banner
+echo -e "======= NITRX INSTALL ======="
 echo -e "$red [$green+$red]$off Nitrx Will Be Installed In Your System";
 echo -e "$red [$green+$red]$off Installing Packages ...";
 pip install -r ./requirements.txt
 echo -e "$red [$green+$red]$off Checking directories ..."
-git pull origin master
 echo -e "$red [$green+$red]$off Installing ...";
 chmod +x Nitrx.py
 echo -e "$red [$green+$red]$off Tool successfully installed and will start in 5s!";
